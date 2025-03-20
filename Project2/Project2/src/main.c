4a5f88a750e363ea6c245aeee581987cbc24c7ff
Isaac <96703341+IsaacNorthrop@users.noreply.github.com>
2025-03-20 16:43:04 -0700

    Update main.c

--- Project2/Project2/src/main.c
+++ Project2/Project2/src/main.c
@@ -137,6 +137,7 @@ functionality.
 /* Standard includes. */
 #include <stdint.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include "stm32f4_discovery.h"
 /* Kernel includes. */
 #include "stm32f4xx.h"
@@ -145,22 +146,14 @@ functionality.
 #include "../FreeRTOS_Source/include/semphr.h"
 #include "../FreeRTOS_Source/include/task.h"
 #include "../FreeRTOS_Source/include/timers.h"
+/* Local includes. */
+#include "../inc/DDS.h"
+#include "../inc/linkedList.h"
 
 
 
 /*-----------------------------------------------------------*/
 #define mainQUEUE_LENGTH 100
-
-#define amber  	0
-#define green  	1
-#define red  	2
-#define blue  	3
-
-#define amber_led	LED3
-#define green_led	LED4
-#define red_led		LED5
-#define blue_led	LED6
-
 
 /*
  * TODO: Implement this function for any hardware specific clock configuration
@@ -172,25 +165,17 @@ static void prvSetupHardware( void );
  * The queue send and receive tasks as described in the comments at the top of
  * this file.
  */
-static void Manager_Task( void *pvParameters );
-static void Blue_LED_Controller_Task( void *pvParameters );
-static void Green_LED_Controller_Task( void *pvParameters );
-static void Red_LED_Controller_Task( void *pvParameters );
-static void Amber_LED_Controller_Task( void *pvParameters );
+static void DDS_Task( void *pvParameters );
+static void Generator_Task( void *pvParameters );
 
-xQueueHandle xQueue_handle = 0;
+xQueueHandle Generator_Queue = 0;
+xQueueHandle User_Defined_Queue = 0;
 
 
 /*-----------------------------------------------------------*/
 
 int main(void)
 {
-
-	/* Initialize LEDs */
-	STM_EVAL_LEDInit(amber_led);
-	STM_EVAL_LEDInit(green_led);
-	STM_EVAL_LEDInit(red_led);
-	STM_EVAL_LEDInit(blue_led);
 
 	/* Configure the system ready to run the demo.  The clock configuration
 	can be done here if it was not done before main() was called. */
@@ -199,17 +184,18 @@ int main(void)
 
 	/* Create the queue used by the queue send and queue receive tasks.
 	http://www.freertos.org/a00116.html */
-	xQueue_handle = xQueueCreate( 	mainQUEUE_LENGTH,		/* The number of items the queue can hold. */
+	Generator_Queue = xQueueCreate( 	mainQUEUE_LENGTH,		/* The number of items the queue can hold. */
 							sizeof( uint16_t ) );	/* The size of each item the queue holds. */
 
-	/* Add to the registry, for the benefit of kernel aware debugging. */
-	vQueueAddToRegistry( xQueue_handle, "MainQueue" );
+	User_Defined_Queue = xQueueCreate( 	mainQUEUE_LENGTH,
+			sizeof( uint16_t ) );
 
-	xTaskCreate( Manager_Task, "Manager", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
-	xTaskCreate( Blue_LED_Controller_Task, "Blue_LED", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
-	xTaskCreate( Red_LED_Controller_Task, "Red_LED", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
-	xTaskCreate( Green_LED_Controller_Task, "Green_LED", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
-	xTaskCreate( Amber_LED_Controller_Task, "Amber_LED", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+	/* Add to the registry, for the benefit of kernel aware debugging. */
+	vQueueAddToRegistry( Generator_Queue, "Generator_Queue" );
+	vQueueAddToRegistry( User_Defined_Queue, "User_Defined_Queue" );
+
+	xTaskCreate( DDS_Task, "DDS", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
+	xTaskCreate( Generator_Task, "Generator", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
 
 	/* Start the tasks and timer running. */
 	vTaskStartScheduler();
@@ -217,146 +203,61 @@ int main(void)
 	return 0;
 }
 
-
 /*-----------------------------------------------------------*/
 
-static void Manager_Task( void *pvParameters )
+static void DDS_Task( void *pvParameters )
 {
-	uint16_t tx_data = amber;
+	int start_time = xTaskGetTickCount();
+	struct taskListNode* activeHead = NULL;
+	struct taskListNode* completedHead = NULL;
+	struct taskListNode* overdueHead = NULL;
 
+	struct taskListNode* temp_node;
+	int temp_execution;
+	int result;
 
 	while(1)
 	{
-
-		if(tx_data == amber)
-			STM_EVAL_LEDOn(amber_led);
-		if(tx_data == green)
-			STM_EVAL_LEDOn(green_led);
-		if(tx_data == red)
-			STM_EVAL_LEDOn(red_led);
-		if(tx_data == blue)
-			STM_EVAL_LEDOn(blue_led);
-
-		if( xQueueSend(xQueue_handle,&tx_data,1000))
+		if( xQueueReceive(Generator_Queue,&temp_node,1000))
 		{
-			printf("Manager: %u ON!\n", tx_data);
-			if(++tx_data == 4)
-				tx_data = 0;
-			vTaskDelay(1000);
+			insertAtEnd(activeHead, temp_node->execution_time, temp_node->period);
+			printf("%d\n", temp_node->execution_time);
 		}
 		else
 		{
 			printf("Manager Failed!\n");
 		}
-	}
-}
-
-/*-----------------------------------------------------------*/
-
-static void Blue_LED_Controller_Task( void *pvParameters )
-{
-	uint16_t rx_data;
-	while(1)
-	{
-		if(xQueueReceive(xQueue_handle, &rx_data, 500))
-		{
-			if(rx_data == blue)
-			{
-				vTaskDelay(250);
-				STM_EVAL_LEDOff(blue_led);
-				printf("Blue Off.\n");
-			}
-			else
-			{
-				if( xQueueSend(xQueue_handle,&rx_data,1000))
-					{
-						printf("BlueTask GRP (%u).\n", rx_data); // Got wwrong Package
-						vTaskDelay(500);
-					}
-			}
+		if(activeHead){
+			activeHead = MergeSort(&activeHead);
 		}
-	}
-}
+		checkOverdue(&activeHead, &overdueHead);
 
-
-/*-----------------------------------------------------------*/
-
-static void Green_LED_Controller_Task( void *pvParameters )
-{
-	uint16_t rx_data;
-	while(1)
-	{
-		if(xQueueReceive(xQueue_handle, &rx_data, 500))
-		{
-			if(rx_data == green)
-			{
-				vTaskDelay(250);
-				STM_EVAL_LEDOff(green_led);
-				printf("Green Off.\n");
-			}
-			else
-			{
-				if( xQueueSend(xQueue_handle,&rx_data,1000))
-					{
-						printf("GreenTask GRP (%u).\n", rx_data); // Got wrong Package
-						vTaskDelay(500);
-					}
-			}
+		if(xQueueSend(User_Defined_Queue, &temp_execution, 1000)){
+			int temp = 0;
 		}
-	}
-}
-
-/*-----------------------------------------------------------*/
-
-static void Red_LED_Controller_Task( void *pvParameters )
-{
-	uint16_t rx_data;
-	while(1)
-	{
-		if(xQueueReceive(xQueue_handle, &rx_data, 500))
-		{
-			if(rx_data == red)
-			{
-				vTaskDelay(250);
-				STM_EVAL_LEDOff(red_led);
-				printf("Red off.\n");
-			}
+		vTaskSuspend(DDS_Task);
+		if(xQueueReceive(User_Defined_Queue, &result, 1000)){
+			if(result)
+				insertAtEnd(completedHead, 0, 0);
 			else
-			{
-				if( xQueueSend(xQueue_handle,&rx_data,1000))
-					{
-						printf("RedTask GRP (%u).\n", rx_data); // Got wrong Package
-						vTaskDelay(500);
-					}
-			}
+				insertAtEnd(overdueHead, 0, 0);
 		}
+		vTaskDelay(1000);
 	}
 }
 
-
-/*-----------------------------------------------------------*/
-
-static void Amber_LED_Controller_Task( void *pvParameters )
+static void Generator_Task( void *pvParameters )
 {
-	uint16_t rx_data;
+
+	char*  tx_data = "fuck";
+
 	while(1)
 	{
-		if(xQueueReceive(xQueue_handle, &rx_data, 500))
+
+		if( xQueueSend(Generator_Queue,&tx_data,1000))
 		{
-			if(rx_data == amber)
-			{
-				vTaskDelay(250);
-				STM_EVAL_LEDOff(amber_led);
-				printf("Amber Off.\n");
-			}
-			else
-			{
-				if( xQueueSend(xQueue_handle,&rx_data,1000))
-					{
-						printf("AmberTask GRP (%u).\n", rx_data); // Got wrong Package
-						vTaskDelay(500);
-					}
-			}
+			printf("successfully sent\n");
+			vTaskDelay(1000);
 		}
 	}
 }
@@ -423,4 +324,3 @@ static void prvSetupHardware( void )
 	/* TODO: Setup the clocks, etc. here, if they were not configured before
 	main() was called. */
 }
-
