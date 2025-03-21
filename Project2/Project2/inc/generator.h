7815962bbbbe75067e03badc6bb733a5d70f348e
Liam Tanner <liamtanner@uvic.ca>
2025-03-20 17:31:50 -0700

    Here is some stuff for you isaac :-()

---
+++ Project2/Project2/inc/generator.h
@@ -0,0 +1,8 @@
+#include ../inc/testBench.h
+#include ../inc/FreeRTOSConfig.h
+#include userDefined.h
+
+
+void Gen1(void *taskParameters);
+void Gen2(void *taskParameters);
+void Gen3(void *taskParameters);
