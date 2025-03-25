ce3c23d31599202353483e7e2f313de4a6d24d7a
Liam Tanner <liamtanner@uvic.ca>
2025-03-25 16:33:11 -0700

    Tasks are periodic now

--- Project2/Project2/src/generator.c
+++ Project2/Project2/src/generator.c
@@ -10,60 +10,72 @@
 
 void Gen1 (void *taskParamters){
 
-	//Create initial FreeRTOS F-Task
-	TaskHandle_t task;
-	xTaskCreate(userTask1, "User_Task_1", configMINIMAL_STACK_SIZE, NULL, 1, &task); // Create task with placeholder priority
+	while (1){
+		//Create initial FreeRTOS F-Task
+		TaskHandle_t task;
+		xTaskCreate(userTask1, "User_Task_1", configMINIMAL_STACK_SIZE, NULL, 1, &task); // Create task with placeholder priority
 
-	if(task == NULL){
-		printf("Error: Gen1 task is NULL\n");
-		return;
+		if(task == NULL){
+			printf("Error: Gen1 task is NULL\n");
+			return;
+		}
+
+		vTaskSuspend(task);
+
+		//Call to our create dd task function happens here
+		createDDTask(task, 1, TASK_1_EXEC, TASK_1_PERIOD);
+
+		vTaskDelay(TASK_1_PERIOD);
+
+
 	}
 
-	vTaskSuspend(task);
-
-	//Call to our create dd task function happens here
-	createDDTask(task, 1, TASK_1_EXEC, TASK_1_PERIOD);
-
-	vTaskDelay(TASK_1_PERIOD);
 }
 
 void Gen2 (void *taskParameters){
 
-	//Create initial FreeRTOS F-Task
-	TaskHandle_t task;
-	xTaskCreate(userTask2, "User_Task_2", configMINIMAL_STACK_SIZE, NULL, 1, &task); // Create task with placeholder priority
+	while (1){
+		//Create initial FreeRTOS F-Task
+		TaskHandle_t task;
+		xTaskCreate(userTask2, "User_Task_2", configMINIMAL_STACK_SIZE, NULL, 1, &task); // Create task with placeholder priority
 
-	if(task == NULL){
-		printf("Error: Gen2 task is NULL\n");
-		return;
+		if(task == NULL){
+			printf("Error: Gen2 task is NULL\n");
+			return;
+		}
+
+		vTaskSuspend(task);
+
+		//Call to our create dd task function happens here
+		createDDTask(task, 2, TASK_2_EXEC, TASK_2_PERIOD);
+
+		vTaskDelay(TASK_2_PERIOD);
 	}
 
-	vTaskSuspend(task);
-
-	//Call to our create dd task function happens here
-	createDDTask(task, 2, TASK_2_EXEC, TASK_2_PERIOD);
-
-	vTaskDelay(TASK_2_PERIOD);
 }
 
 void Gen3 (void *taskParameters){
 
-	//Create initial FreeRTOS F-Task
-	TaskHandle_t task;
-	xTaskCreate(userTask3, "User_Task_3", configMINIMAL_STACK_SIZE, NULL, 1, &task); // Create task with placeholder priority
+	while(1){
+		//Create initial FreeRTOS F-Task
+		TaskHandle_t task;
+		xTaskCreate(userTask3, "User_Task_3", configMINIMAL_STACK_SIZE, NULL, 1, &task); // Create task with placeholder priority
 
-	if(task == NULL){
-		printf("Error: Gen3 task is NULL\n");
-		return;
+		if(task == NULL){
+			printf("Error: Gen3 task is NULL\n");
+			return;
+		}
+
+		vTaskSuspend(task);
+
+		//Call to our create dd task function happens here
+		createDDTask(task, 3, TASK_3_EXEC, TASK_3_PERIOD);
+
+
+		vTaskDelay(TASK_3_PERIOD);
 	}
 
-	vTaskSuspend(task);
 
-	//Call to our create dd task function happens here
-	createDDTask(task, 3, TASK_3_EXEC, TASK_3_PERIOD);
-
-
-	vTaskDelay(TASK_3_PERIOD);
 }
 
 void createDDTask (TaskHandle_t task, int executionTime, int period, int id){
