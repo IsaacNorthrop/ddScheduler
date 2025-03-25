cd8f6710bc9b23a9ebb8570f64907d811213cf39
Isaac <96703341+IsaacNorthrop@users.noreply.github.com>
2025-03-25 16:36:40 -0700

    delete nodes from active list properly

--- Project2/Project2/src/DDS.c
+++ Project2/Project2/src/DDS.c
@@ -3,12 +3,15 @@
 void checkOverdue(struct taskListNode** active, struct taskListNode** overdue){
     if (*head == NULL)
         return;
-
+    int position = 0;
     struct taskListNode* temp = *active;
     while (temp->next != NULL) {
     	int current_time = xTaskGetTickCount();
-    	if(current_time > temp->deadline)
-    		insertAtEnd(overdue, &temp);
+    	if(current_time > temp->deadline){
+            insertAtEnd(overdue, &temp);
+            deletefromPosition(active, position);
+        }
+    	position++;
         temp = temp->next;
     }
     return;
